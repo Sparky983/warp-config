e5347224752832449a6aa01c62dc67ee590f3c03
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-07-22 17:33:24 +1000

    Remove redundant `@SuppressWarnings`

--- warp-api/src/main/java/me/sparky983/warp/internal/Deserializer.java
+++ warp-api/src/main/java/me/sparky983/warp/internal/Deserializer.java
@@ -17,7 +17,6 @@ import org.jspecify.annotations.NullMarked;
  *
  * @param <T> the deserialized type
  */
-@SuppressWarnings("OverlyLongLambda")
 @FunctionalInterface
 @NullMarked
 public interface Deserializer<T> {
