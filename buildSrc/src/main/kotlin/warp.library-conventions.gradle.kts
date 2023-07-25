9aff493a0a916b434d5807e42ec9302f8bfa3b26
Sparky <87631423+Sparky983@users.noreply.github.com>
2023-07-25 22:12:30 +1000

    Update to Java 20

--- buildSrc/src/main/kotlin/warp.library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/warp.library-conventions.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion.set(JavaLanguageVersion.of(20))
     }
     withJavadocJar()
     withSourcesJar()
