f6d31b68bb5c0ca268dbcfd3ce703502c66f7953
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-07-29 13:58:20 +1000

    Add nil node equals and hash code tests

--- warp/src/acceptanceTest/java/me/sparky983/warp/nodes/NilNodeTest.java
+++ warp/src/acceptanceTest/java/me/sparky983/warp/nodes/NilNodeTest.java
@@ -1,6 +1,7 @@
 package me.sparky983.warp.nodes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import me.sparky983.warp.ConfigurationNode;
@@ -8,16 +9,27 @@ import org.junit.jupiter.api.Test;
 
 class NilNodeTest {
   @Test
-  void testNil() {
-    final ConfigurationNode.Nil nil = ConfigurationNode.nil();
+  void testEquals_DifferentType() {
+    assertNotEquals(ConfigurationNode.nil(), new Object());
+  }
 
-    assertNotNull(nil);
+  @Test
+  void testEquals_Same() {
+    assertEquals(ConfigurationNode.nil(), ConfigurationNode.nil());
+  }
+
+  @Test
+  void testEquals_SameType() {
+    assertEquals(ConfigurationNode.nil(), new ConfigurationNode.Nil() {});
+  }
+
+  @Test
+  void testHashCode() {
+    assertEquals(0, ConfigurationNode.nil().hashCode());
   }
 
   @Test
   void testToString() {
-    final ConfigurationNode.Nil nil = ConfigurationNode.nil();
-
-    assertEquals("nil", nil.toString());
+    assertEquals("nil", ConfigurationNode.nil().toString());
   }
 }
