3e761ce086c3592027ae8fa3e0e7925c329fa9de
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-09-24 16:27:47 +1000

    Replace `StaticRenderer` with lambda

--- warp/src/main/java/me/sparky983/warp/Renderer.java
+++ warp/src/main/java/me/sparky983/warp/Renderer.java
@@ -1,11 +1,18 @@
 package me.sparky983.warp;
 
+import java.util.Objects;
 import me.sparky983.warp.internal.StaticRenderer;
 import org.jspecify.annotations.Nullable;
 
 public interface Renderer<T extends @Nullable Object> {
   static <T extends @Nullable Object> Renderer<T> of(final T value) {
-    return new StaticRenderer<>(value);
+    Objects.requireNonNull(value, "value cannot be null");
+
+    return (context) -> {
+      Objects.requireNonNull(context, "context cannot be null");
+
+      return value;
+    };
   }
 
   T render(Context context);
