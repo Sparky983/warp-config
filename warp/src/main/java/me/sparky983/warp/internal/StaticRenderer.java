c87dc74d7b9d86a9340c7f9b74996ecc157ee407
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-09-24 16:07:57 +1000

    record to class

--- warp/src/main/java/me/sparky983/warp/internal/StaticRenderer.java
+++ warp/src/main/java/me/sparky983/warp/internal/StaticRenderer.java
@@ -4,7 +4,15 @@ import java.util.Objects;
 import me.sparky983.warp.Renderer;
 import org.jspecify.annotations.Nullable;
 
-public record StaticRenderer<T extends @Nullable Object>(T value) implements Renderer<T> {
+public final class StaticRenderer<T extends @Nullable Object> implements Renderer<T> {
+  private final T value;
+
+  public StaticRenderer(final T value) {
+    Objects.requireNonNull(value, "value cannot be null");
+
+    this.value = value;
+  }
+
   @Override
   public @Nullable T render(final Context context) {
     Objects.requireNonNull(context, "context cannot be null");
