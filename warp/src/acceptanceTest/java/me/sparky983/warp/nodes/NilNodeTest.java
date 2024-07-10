88ef914224d7cad774dd255f7f7834edde89ea57
Sparky <87631423+Sparky983@users.noreply.github.com>
2024-07-10 15:54:11 +1000

    format

--- warp/src/acceptanceTest/java/me/sparky983/warp/nodes/NilNodeTest.java
+++ warp/src/acceptanceTest/java/me/sparky983/warp/nodes/NilNodeTest.java
@@ -1,7 +1,6 @@
 package me.sparky983.warp.nodes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import me.sparky983.warp.ConfigurationNode;
 import org.junit.jupiter.api.Test;
