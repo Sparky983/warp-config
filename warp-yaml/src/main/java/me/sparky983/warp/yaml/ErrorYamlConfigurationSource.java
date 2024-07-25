a8d5d9b305ce7755ba9dd2ae20d6f491b62adf27
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2024-07-25 17:13:12 +1000

    Update ErrorYamlConfigurationSource.java

--- warp-yaml/src/main/java/me/sparky983/warp/yaml/ErrorYamlConfigurationSource.java
+++ warp-yaml/src/main/java/me/sparky983/warp/yaml/ErrorYamlConfigurationSource.java
@@ -13,7 +13,7 @@ import me.sparky983.warp.ConfigurationNode;
 record ErrorYamlConfigurationSource(ConfigurationException exception)
     implements YamlConfigurationSource {
   /**
-   * Constructs a {@link ErrorYamlConfigurationSource}.
+   * Constructs a {@code ErrorYamlConfigurationSource}.
    *
    * @param exception the exception
    * @throws NullPointerException if {@code exception} is {@code null}
