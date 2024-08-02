23e398341a8c2571209dc451142955c1b51ad016
Sparky <87631423+Sparky983@users.noreply.github.com>
2024-08-03 08:57:28 +1000

    Make configuration deserializer fail fast (#56)

--- warp/src/main/java/me/sparky983/warp/internal/DefaultsRegistry.java
+++ warp/src/main/java/me/sparky983/warp/internal/DefaultsRegistry.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import me.sparky983.warp.ConfigurationNode;
+import me.sparky983.warp.Renderer;
 
 /** A registry of defaults. */
 public final class DefaultsRegistry {
-  private final Map<Class<?>, ConfigurationNode> defaults = new HashMap<>();
+  private final Map<Class<?>, Renderer<?>> defaults = new HashMap<>();
 
   private DefaultsRegistry() {}
 
@@ -22,35 +22,39 @@ public final class DefaultsRegistry {
   }
 
   /**
-   * Registers a default for the given type.
+   * Registers a default renderer for the given type.
    *
-   * @param type the type
-   * @param node the default value
+   * @param type the type to register the default for
+   * @param defaultRenderer the default renderer
    * @return this registry
-   * @throws IllegalStateException if a default for the given type is already registered.
-   * @throws NullPointerException if the type or the node are {@code null}.
+   * @throws IllegalStateException if a default renderer for the given type is already registered.
+   * @throws NullPointerException if the type or the renderer are {@code null}.
+   * @param <T> the type to register the default for
    */
-  public DefaultsRegistry register(final Class<?> type, final ConfigurationNode node) {
+  public <T> DefaultsRegistry register(
+      final Class<T> type, final Renderer<? extends T> defaultRenderer) {
     Objects.requireNonNull(type, "type cannot be null");
-    Objects.requireNonNull(node, "node cannot be null");
+    Objects.requireNonNull(defaultRenderer, "defaultRenderer cannot be null");
 
-    if (defaults.putIfAbsent(type, node) != null) {
+    if (defaults.putIfAbsent(type, defaultRenderer) != null) {
       throw new IllegalStateException("Default for type " + type + " already registered");
     }
     return this;
   }
 
   /**
-   * Returns the default for the given type.
+   * Returns the default renderer for the given type.
    *
    * @param type the type
-   * @return an {@link Optional} containing the default for the specified type if one exists,
-   *     otherwise an {@linkplain Optional#empty() empty optional}.
+   * @return an {@link Optional} containing the default renderer for the specified type if one
+   *     exists, otherwise an {@linkplain Optional#empty() empty optional}.
    * @throws NullPointerException if the type is {@code null}.
+   * @param <T> the type
    */
-  public Optional<ConfigurationNode> get(final Class<?> type) {
+  @SuppressWarnings("unchecked")
+  public <T> Optional<Renderer<? extends T>> get(final Class<? extends T> type) {
     Objects.requireNonNull(type, "type cannot be null");
 
-    return Optional.ofNullable(defaults.get(type));
+    return Optional.ofNullable((Renderer<? extends T>) defaults.get(type));
   }
 }
