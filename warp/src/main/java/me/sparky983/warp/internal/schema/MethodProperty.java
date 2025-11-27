c32ed10bcfc8b6770d83ceb4bd4ab90f495e88eb
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2025-11-27 21:35:10 +1100

    Add static check
    Used to be caught under the abstract check

--- warp/src/main/java/me/sparky983/warp/internal/schema/MethodProperty.java
+++ warp/src/main/java/me/sparky983/warp/internal/schema/MethodProperty.java
@@ -34,6 +34,10 @@ final class MethodProperty<T> implements Schema.Property<T> {
     if (!Modifier.isPublic(method.getModifiers())) {
       throw new IllegalArgumentException("Method " + method + " must be public");
     }
+    
+    if (Modifier.isStatic(method.getModifiers())) {
+      throw new IllegalArgumentException("Method " + method + " must be non-static");
+    }
 
     if (method.getParameterCount() != 0) {
       throw new IllegalArgumentException("Method " + method + " must not declare any parameters");
