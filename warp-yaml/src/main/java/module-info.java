4e23bd9f39487fd5902e546cf2197f3883bf48f0
Sparky <87631423+Sparky983@users.noreply.github.com>
2025-12-19 17:55:37 +1100

    Add module Javadoc and suppress name warning

--- warp-yaml/src/main/java/module-info.java
+++ warp-yaml/src/main/java/module-info.java
@@ -1,5 +1,12 @@
+import me.sparky983.warp.yaml.YamlConfigurationSource;
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * Warp YAML provides a YAML configuration source.
+ *
+ * @see YamlConfigurationSource
+ */
+@SuppressWarnings("module")
 @NullMarked
 module me.sparky983.warp.yaml {
   requires transitive me.sparky983.warp;
--- warp/src/main/java/module-info.java
+++ warp/src/main/java/module-info.java
@@ -1,5 +1,12 @@
+import me.sparky983.warp.Warp;
 import org.jspecify.annotations.NullMarked;
 
+/**
+ * Warp is a powerful annotation-based configuration library.
+ *
+ * @see Warp
+ */
+@SuppressWarnings("module")
 @NullMarked
 module me.sparky983.warp {
   requires static org.jetbrains.annotations;
